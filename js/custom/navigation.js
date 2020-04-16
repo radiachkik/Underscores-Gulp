0160dc6daf4dce859f607cd33b49b0dda801b4e7
radiachk <Ra21022000>
2020-04-16 19:58:54 +0200

    Header is customized and adjusted for every screen size

--- js/custom/navigation.js
+++ js/custom/navigation.js
@@ -33,10 +33,8 @@
 	button.onclick = function() {
 
 		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
 			button.setAttribute( 'aria-expanded', 'false' );
 		} else {
-			container.className += ' toggled';
 			button.setAttribute( 'aria-expanded', 'true' );
 		}
 
