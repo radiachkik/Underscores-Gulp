e9f25215d51143c2d8086abd71842df65689b6bb
radiachk <Ra21022000>
2020-04-16 20:07:11 +0200

    Customized

--- footer.php
+++ footer.php
@@ -15,17 +15,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'homepage' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'homepage' ), 'WordPress' );
-				?>
+			<a href="/">
+                Radi Achkik © 2020
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'homepage' ), 'homepage', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
