1b15a4ad6af189aee65b9dff90d4108789b0921a
radiachk <Ra21022000>
2020-04-26 01:33:50 +0200

    set animation hooks only on home page

--- js/custom/main.js
+++ js/custom/main.js
@@ -8,72 +8,76 @@ window.onload = function() {
         sizeVariations: 5,
     });
 
-    var controller = new ScrollMagic.Controller();
+    var landingPage = document.querySelector('body.home');
 
-    new ScrollMagic.Scene({
-        triggerElement: ".skill-microcontroller",
-        triggerHook: 0.7,
-    })
-        .setClassToggle(".skill-microcontroller", "visible")
-        .addTo(controller);
+    if (landingPage) {
+        var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        triggerElement: ".skill-software",
-        triggerHook: 0.7,
-    })
-        .setClassToggle(".skill-software", "visible")
-        .addTo(controller);
+        new ScrollMagic.Scene({
+            triggerElement: ".skill-microcontroller",
+            triggerHook: 0.7,
+        })
+            .setClassToggle(".skill-microcontroller", "visible")
+            .addTo(controller);
 
-    new ScrollMagic.Scene({
-        triggerElement: ".skill-networking",
-        triggerHook: 0.7,
-    })
-        .setClassToggle(".skill-networking", "visible")
-        .addTo(controller);
+        new ScrollMagic.Scene({
+            triggerElement: ".skill-software",
+            triggerHook: 0.7,
+        })
+            .setClassToggle(".skill-software", "visible")
+            .addTo(controller);
 
-
-    new ScrollMagic.Scene({
-        triggerElement: "#service-consulting",
-        triggerHook: 0.9,
-    })
-        .setClassToggle("#service-consulting", "visible")
-        .addTo(controller);
+        new ScrollMagic.Scene({
+            triggerElement: ".skill-networking",
+            triggerHook: 0.7,
+        })
+            .setClassToggle(".skill-networking", "visible")
+            .addTo(controller);
 
 
-    new ScrollMagic.Scene({
-        triggerElement: "#service-development",
-        triggerHook: 0.9,
-    })
-        .setClassToggle("#service-development", "visible")
-        .addTo(controller);
+        new ScrollMagic.Scene({
+            triggerElement: "#service-consulting",
+            triggerHook: 0.9,
+        })
+            .setClassToggle("#service-consulting", "visible")
+            .addTo(controller);
 
-    new ScrollMagic.Scene({
-        triggerElement: "#service-warranty",
-        triggerHook: 0.9,
-    })
-        .setClassToggle("#service-warranty", "visible")
-        .addTo(controller);
 
-    new ScrollMagic.Scene({
-        triggerElement: "#service-deployment",
-        triggerHook: 0.9,
-    })
-        .setClassToggle("#service-deployment", "visible")
-        .addTo(controller);
+        new ScrollMagic.Scene({
+            triggerElement: "#service-development",
+            triggerHook: 0.9,
+        })
+            .setClassToggle("#service-development", "visible")
+            .addTo(controller);
 
-    $('#contact input').change(function (e) {
-        if (e.target.value === "") {
-            e.target.style.borderColor = "white";
-        } else {
-            e.target.style.borderColor = "#00a8cc";
-        }
-    });
+        new ScrollMagic.Scene({
+            triggerElement: "#service-warranty",
+            triggerHook: 0.9,
+        })
+            .setClassToggle("#service-warranty", "visible")
+            .addTo(controller);
 
-    $('#contact textarea').change(function (e) {
-        if (e.target.value === "") {
-            e.target.style.borderColor = "white";
-        } else {
-            e.target.style.borderColor = "#00a8cc";
-        }
-    });
+        new ScrollMagic.Scene({
+            triggerElement: "#service-deployment",
+            triggerHook: 0.9,
+        })
+            .setClassToggle("#service-deployment", "visible")
+            .addTo(controller);
+
+        $('#contact input').change(function (e) {
+            if (e.target.value === "") {
+                e.target.style.borderColor = "white";
+            } else {
+                e.target.style.borderColor = "#00a8cc";
+            }
+        });
+
+        $('#contact textarea').change(function (e) {
+            if (e.target.value === "") {
+                e.target.style.borderColor = "white";
+            } else {
+                e.target.style.borderColor = "#00a8cc";
+            }
+        });
+    }
 };
