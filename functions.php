b8dff7b95c8f0d1d936ab9f740c1a8c9ae25e1ad
radiachk <Ra21022000>
2020-04-26 13:51:43 +0200

    remove security policy

--- functions.php
+++ functions.php
@@ -153,21 +153,7 @@ function homepage_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'homepage_scripts' );
 
-/**
- * Add security policy
- */
-function send_security_header ( ) {
-    header ( "Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' https:; object-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self'; child-src 'self' https:; font-src 'self' data:; connect-src 'self'" );
-    header ( "X-Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' https:; object-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self'; child-src 'self' https:; font-src 'self' data:; connect-src 'self'" );
-    header ( "X-WebKit-CSP: default-src 'self'; script-src 'self' 'unsafe-inline' https:; object-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self'; frame-src 'self' https:; font-src 'self' data:; connect-src 'self'" );
-    header ( "X-Content-Type-Options: nosniff" );
-    header ( "X-Frame-Options: sameorigin" );
-    header ( "X-XSS-Protection: 1; mode=block" );
-    header ( "Strict-Transport-Security: max-age=31536000" );
-    header ( "X-Permitted-Cross-Domain-Policies: none" );
-    header ( "Referrer-Policy: no-referrer-when-downgrade" );
-}
-add_action ( 'send_headers', 'send_security_header' );
+
 
 /**
  * Implement the Custom Header feature.
