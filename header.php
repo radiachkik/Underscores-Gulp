823d40e6064b16154d70f1bab49b1c3feda89e5d
radiachk <Ra21022000>
2020-04-26 12:57:07 +0200

    added csp support

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
